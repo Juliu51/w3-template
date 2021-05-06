8766e367f9803d2991f8254dd44b3daa1ed61593
Julius <80947873+Juliu51@users.noreply.github.com>
2021-05-06 16:56:59 +0300

    done

--- js/main.js
+++ js/main.js
@@ -2,24 +2,51 @@
 
 let menuPirmas = document.querySelector('.menu');
 let menu = document.querySelector('.container');
+let menuAntras = document.querySelector('.contact');
+let isjungtiContact = document.querySelector('.contact_lentele');
+let isjungtiMenu = document.querySelector('.menu_lentele');
 
 menuPirmas.addEventListener('click', () => {
 
     if (menu.className == 'container on') {
-        console.log(true);
+        isjungtiContact.classList.remove('off');
     } else {
         menu.classList.add('on');
+        isjungtiContact.classList.add('off');
     }
 });
-console.log(menu);
 
 let isjungti = document.querySelector('.isjungti');
 
 isjungti.addEventListener('click', () => {
     if (menu.className == 'container on') {
         menu.classList.remove('on');
+        isjungtiContact.classList.remove('off');
     } else {
         menu.classList.add('on');
+        isjungtiContact.classList.add('off');
     }
 });
-console.log(isjungti);
+
+menuAntras.addEventListener('click', () => {
+
+    if (menu.className == 'container on') {
+        isjungtiMenu.classList.remove('off');
+    } else {
+        menu.classList.add('on');
+        isjungtiMenu.classList.add('off');
+    }
+});
+
+let ijungti = document.querySelector('.ijungti');
+
+ijungti.addEventListener('click', () => {
+    if (menu.className == 'container on') {
+        menu.classList.remove('on');
+        isjungtiMenu.classList.remove('off');
+    } else {
+        menu.classList.add('on');
+        isjungtiMenu.classList.add('off');
+    }
+});
+
